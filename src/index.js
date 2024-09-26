08fb3ddc835687a517736c93a5146bee0820131c
Atikur Rahman <atikur2013rahman@gmail.com>
2024-09-26 12:24:08 +0600

    components

--- src/index.js
+++ src/index.js
@@ -17,6 +17,14 @@ function Cat2() {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
+    <h1>Welcome</h1>
+    <h2>To do App</h2>
+    <p>
+      ReactJS is a JavaScript library used to build User Interfaces(UI).Run this
+      command to create a React application named my-react-app: npx
+      create-react-app my-react-app
+    </p>
+
     <Cat />
     <Cat2 />
   </div>
