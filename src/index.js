3da3e67e3e4ed1de654ae84ec6e8bae146bfbeb0
Atikur Rahman <atikur2013rahman@gmail.com>
2024-09-26 11:50:45 +0600

    starting

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <h1>Welcome</h1>
+    <h2>To do App</h2>
+    <p>
+      ReactJS is a JavaScript library used to build User Interfaces(UI).Run this
+      command to create a React application named my-react-app: npx
+      create-react-app my-react-app
+    </p>
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
